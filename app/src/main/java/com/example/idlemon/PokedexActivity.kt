db3da5801373990ff48d20b6ee825189aed12502
Sami Hajadi <sami.hajadi@etu.univ-lemans.fr>
2026-03-20 13:38:05 +0100

    modif couleur spinner pokédex

--- app/src/main/java/com/example/idlemon/PokedexActivity.kt
+++ app/src/main/java/com/example/idlemon/PokedexActivity.kt
@@ -62,24 +62,24 @@ class PokedexActivity : BaseActivity() {
         chkOwned = findViewById(R.id.chkOwned)
         recyclerView = findViewById(R.id.pokedexRecyclerView)
 
-        // 5 colonnes
+        //5 colonnes
         recyclerView.layoutManager = GridLayoutManager(this, 5)
     }
 
     private fun chargerDonnees() {
-        // 1. IDs possédés
+        //IDs possédés
         idsPossedes.clear()
         Player.getEquipe().forEach { idsPossedes.add(it.species.num) }
         Player.getBoxPokemon().forEach { idsPossedes.add(it.species.num) }
 
-        // 2. Toutes les espèces
+        //Toutes les espèces
         toutesLesEspeces = DataManager.model.getAllPokemonSpecies().sortedBy { it.num }
         especesFiltrees = toutesLesEspeces
 
-        // 3. Mise à jour du compteur global (X possédés sur Y total)
+        //Maj du compteur (10 / 900 capturés)
         txtCounter.text = "${idsPossedes.size} / ${toutesLesEspeces.size} capturés"
 
-        // 4. Initialisation de l'Adapter
+        //initialisation de l'Adapter
         adapter = PokedexAdapter(especesFiltrees, idsPossedes) { species, estPossede ->
             afficherPopupPokemon(species, estPossede)
         }
@@ -87,35 +87,45 @@ class PokedexActivity : BaseActivity() {
     }
 
     private fun setupFiltres() {
-        // --- Setup du Spinner (Liste des types) ---
+        //setup type
         val types = listOf(
             "Tous les types", "Acier", "Combat", "Dragon", "Eau", "Electrik",
             "Fee", "Feu", "Glace", "Insecte", "Normal", "Plante", "Poison",
             "Psy", "Roche", "Sol", "Spectre", "Tenebre", "Vol"
         )
-        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, types)
+        //Pour forcer le txt noir
+        val spinnerAdapter = object : ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, types) {
+            override fun getView(position: Int, convertView: View?, parent: android.view.ViewGroup): View {
+                val view = super.getView(position, convertView, parent) as TextView
+                view.setTextColor(Color.BLACK)
+                return view
+            }
+        }
+
+        //utilise le xml item spinner
+        spinnerAdapter.setDropDownViewResource(R.layout.item_spinner_pokedex)
+
+        //attache l'adapter au spinner
         spinnerType.adapter = spinnerAdapter
 
-        // --- Listeners ---
-
-        // Listener pour la recherche de texte
+        //Listener pour la recherche de texte
         searchBar.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) { appliquerFiltres() }
         })
 
-        // Listener pour le dropdown de type
+        //Listener pour le dropdown de type
         spinnerType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) { appliquerFiltres() }
             override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
 
-        // Listener pour la case à cocher "Possédés"
+        //Listener pour la case à cocher "Possédés"
         chkOwned.setOnCheckedChangeListener { _, _ -> appliquerFiltres() }
     }
 
-    // Le cœur du système : combine tous les filtres en même temps !
+    //Application des filtres
     private fun appliquerFiltres() {
         val texte = searchBar.text.toString().trim().lowercase()
         val typeSelectionne = spinnerType.selectedItem.toString()
@@ -123,23 +133,23 @@ class PokedexActivity : BaseActivity() {
 
         especesFiltrees = toutesLesEspeces.filter { species ->
 
-            // 1. Filtre Texte
+            //Filtre Texte
             val correspondTexte = texte.isEmpty() ||
                     species.nom.lowercase().contains(texte) ||
                     species.num.toString() == texte
 
-            // 2. Filtre Type
+            //Filtre Type
             val correspondType = typeSelectionne == "Tous les types" ||
                     species.type.any { it.nom.equals(typeSelectionne, ignoreCase = true) }
 
-            // 3. Filtre Possédé
+            //Filtre Possédé
             val correspondPossede = !seulementPossedes || idsPossedes.contains(species.num)
 
-            // On garde le Pokémon SEULEMENT s'il passe les 3 tests
+            //verif si 3 filtres sont vrai
             correspondTexte && correspondType && correspondPossede
         }
 
-        // On envoie la liste filtrée à l'adapter
+        //uodate liste filtrée
         adapter.updateData(especesFiltrees)
     }
 
@@ -166,7 +176,7 @@ class PokedexActivity : BaseActivity() {
         if (estPossede) {
             // --- POKÉMON DÉCOUVERT ---
             txtName.text = species.nom
-            txtDesc.text = "✔️ Enregistré dans le Pokédex"
+            txtDesc.text = "Enregistré dans le Pokédex"
 
             // Rareté avec couleur
             txtRarete.text = species.rarete
@@ -183,11 +193,11 @@ class PokedexActivity : BaseActivity() {
                 type2.visibility = View.GONE
             }
 
-            // --- CHRONOLOGIE D'ÉVOLUTION ---
+            //chronologie d'évolution
             separator.visibility = View.VISIBLE
             evoTitle.visibility = View.VISIBLE
 
-            // Pré-évolution
+            //Pré-évolution
             if (species.prevo != null) {
                 txtPrevo.text = "← Évolue depuis : ${species.prevo}"
                 txtPrevo.visibility = View.VISIBLE
@@ -195,7 +205,7 @@ class PokedexActivity : BaseActivity() {
                 txtPrevo.visibility = View.GONE
             }
 
-            // Évolution future
+            //Évolution
             if (species.evo != null) {
                 val nivText = if (species.evoLevel != null) " (Niv. ${species.evoLevel})" else ""
                 txtEvo.text = "→ Évolue en : ${species.evo}$nivText"
@@ -204,21 +214,21 @@ class PokedexActivity : BaseActivity() {
                 txtEvo.visibility = View.GONE
             }
 
-            // S'il n'a ni pré-évolution ni évolution (Ex: Tauros, Légendaires)
+            //Ni l'un ni l'autre
             if (species.prevo == null && species.evo == null) {
                 txtEvo.text = "Stade unique (Pas d'évolution)"
                 txtEvo.visibility = View.VISIBLE
             }
 
-            // Image HD Couleur
+            //Img couleur
             imgPoke.clearColorFilter()
             imgPoke.alpha = 1.0f
             Glide.with(this).load(DataManager.model.getFrontSprite(species.num)).into(imgPoke)
 
         } else {
-            // --- POKÉMON INCONNU ---
+            //Poké inconnu
             txtName.text = "???"
-            txtDesc.text = "❌ Espèce inconnue. Continuez de jouer pour l'obtenir !"
+            txtDesc.text = "Espèce inconnue. Continuez de jouer pour l'obtenir !"
 
             txtRarete.visibility = View.GONE
             type1.visibility = View.GONE
@@ -228,7 +238,7 @@ class PokedexActivity : BaseActivity() {
             separator.visibility = View.GONE
             evoTitle.visibility = View.GONE
 
-            // Effet Grisé + Transparent
+            //grisé + alpha
             val matrix = ColorMatrix().apply { setSaturation(0f) }
             imgPoke.colorFilter = ColorMatrixColorFilter(matrix)
             imgPoke.alpha = 0.5f
@@ -240,13 +250,13 @@ class PokedexActivity : BaseActivity() {
     }
     private fun getColorForRarity(rarete: String): Int {
         return when (rarete.lowercase()) {
-            "commun" -> Color.parseColor("#A0A0A0")     // Gris
-            "peu commun" -> Color.parseColor("#4CAF50") // Vert
-            "rare" -> Color.parseColor("#2196F3")       // Bleu
-            "epique", "épique" -> Color.parseColor("#9C27B0") // Violet
-            "fabuleux" -> Color.parseColor("#E91E63")   // Rose/Rouge
-            "legendaire", "légendaire" -> Color.parseColor("#FFC107") // Doré
-            else -> Color.parseColor("#333333") // Sombre par défaut
+            "commun" -> Color.parseColor("#A0A0A0") //Gris
+            "peu commun" -> Color.parseColor("#4CAF50") //Vert
+            "rare" -> Color.parseColor("#2196F3") //Bleu
+            "epique", "épique" -> Color.parseColor("#9C27B0") //Violet
+            "fabuleux" -> Color.parseColor("#E91E63") // Rose/Rouge
+            "legendaire", "légendaire" -> Color.parseColor("#FFC107") //Doré
+            else -> Color.parseColor("#000000")
         }
     }
 }
